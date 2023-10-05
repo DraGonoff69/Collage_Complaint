f39cd9bd1bfc0c86f5c9072882a4979c7af7f0d1
Ayush Mishra <100191388+DraGonoff69@users.noreply.github.com>
2023-10-05 21:02:31 +0530

    finallllee

--- vite.config.js
+++ vite.config.js
@@ -18,57 +18,15 @@ const manifestForPlugin = {
     "maskable_icon.png",
   ],
   manifest: {
-    name: "MobilEASE - Mobile Efficient Assistance for Traffic",
-    short_name: "MobilEASE",
-    description: "Application to report traffic issues around you",
+    name: "College Complaint Web App",
+    short_name: "Complaint",
+    description: "Application",
     icons: [
       {
         src: "/logo.png",
         sizes: "512x512",
         type: "image/png",
-      },
-      {
-        src: "/maskable_icon_x48.png",
-        sizes: "48x48",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "/maskable_icon_x72.png",
-        sizes: "72x72",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "/maskable_icon_x96.png",
-        sizes: "96x96",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "/maskable_icon_x128.png",
-        sizes: "128x128",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "/maskable_icon_x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "/maskable_icon_x384.png",
-        sizes: "384x384",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "/maskable_icon_x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable",
-      },
+      }
     ],
     theme_color: "#212121",
     background_color: "#D7FFFE",
